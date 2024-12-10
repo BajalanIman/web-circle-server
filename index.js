7c55762ae380765dd32b5ef098fa920fee214c2a
Iman <bajalaniman@gmail.com>
2024-12-10 02:10:07 +0100

    adding cors

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: "f24-web-circle-berlin.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use(express.json());
